094bd34fcd90613550561f7a7a71caf0f302d96d
Mauricio B de Paula <Mauricio@MauricisMacBook.hitronhub.home>
2018-01-04 01:03:07 -0800

    Storage implementation started. Needs proper formatting and retrieve attributes from user.

--- src/pages/addnew/addnew.ts
+++ src/pages/addnew/addnew.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { AppData } from '../../back/app-data';
+import { Message } from '../../back/app-data';
+
 /**
  * Generated class for the AddnewPage page.
  *
@@ -14,12 +17,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'addnew.html',
 })
 export class AddnewPage {
+  private appData: AppData = new AppData();
+  private messages: Message[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddnewPage');
+  }
+
+  addEntry(): void {
+    this.appData.addEntry('Today', 13, 30, 'This is my message body.');
+    console.log("Add entry method called");
+  }
+
+  getEntry(): void{
+    this.appData.getEntries().then((entries) => {
+      this.messages = entries;
+    });
+    console.log("Get entry method called")
+    console.log(this.messages)
   }
 
 }
