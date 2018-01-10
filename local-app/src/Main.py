7adfaaa1c2df0b7e8bd1ad8eadccb6df8ab6627e
Mauricio B de Paula <Mauricio@w134-87-152-034.wireless.uvic.ca>
2018-01-10 11:56:05 -0800

    Uploading files for the local-app that will work as the assistant.

---
+++ local-app/src/Main.py
@@ -0,0 +1,69 @@
+import boto3
+import os
+import time
+import datetime
+from pygame import mixer
+from Downloader import Downloader
+from Messages import Messages
+
+class Main:
+    # Initialize audio player
+    mixer.init()
+
+    ####################################################
+    # Methods responsible in calling play_audio method #
+    ####################################################
+    # Method says greetings. 3 Options: Morning, Afternoon, Night
+    # AfternoonBeing and AfternoonEnd determine when to switch greetings
+    # time is an array in format (HOUR, MINUTE)
+    @staticmethod
+    def say_greetings(time):
+        if time[0] < Messages.AfternoonBegin:
+            play_audio("Greetings/Morning")
+        elif time[0] < Messages.AfternoonEnd:
+            play_audio("Greetings/Afternoon")
+        else:
+            play_audio("Greetings/Night")
+    # Method says meal warnings. 2 Options: Lunch and Dinner
+    # Lunch[0] holds time of lunch and Dinner[0] holds time of dinner
+    # time is an array in format (HOUR, MINUTE)
+    @staticmethod
+    def say_meal_warning(time):
+        if time[0] <= Messages.Lunch[0]:
+            play_audio("Meals/Lunch")
+        elif time[0] <= Messages.Dinner[0]:
+            play_audio("Meals/Dinner")
+    # Method says the current day of the week
+    # Monday = 0 and Sunday = 6
+    # day holds the index
+    @staticmethod
+    def say_weekday(day):
+        play_audio("/Days/" + Message.days[0])
+        return
+
+    # Method obtains current day of the week from system
+    # Returns number of the weekday (Monday = 0 ... Sunday = 6)
+    @staticmethod
+    def get_weekday():
+        return datetime.datetime.today().weekday()
+
+    # Method obtains current time from system
+    # Returns (HOUR, MINUTE)
+    @staticmethod
+    def get_time():
+        time_tuple = time.localtime(time.time())
+        return (time_tuple[3], time_tuple[4])
+
+    # Method loads to mixer and plays audio file given a path
+    # If audio file does not exist, it returns out of the method
+    @staticmethod
+    def play_audio(path):
+        if not Downloader.check_path("../Audio/" + path + ".ogg"):
+            print("There was an error loading and playing the file.\nFile might not exist.")
+            return
+        mixer.music.load("../Audio/" + path + ".ogg")
+        mixer.music.play()
+        while mixer.music.get_busy == True:
+            pass
+
+    #Close mixer
