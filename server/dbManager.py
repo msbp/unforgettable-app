d7c709f7de11fea4b67618a5e1418aa170fce5d2
Mauricio B de Paula <Mauricio@w134-87-157-033.wireless.uvic.ca>
2018-01-16 12:00:21 -0800

    Added more functionality to the database manager file.

--- server/dbManager.py
+++ server/dbManager.py
@@ -1,5 +1,26 @@
-from server import app
+from server import db
 from models import Messages
 
+# This method creates tables from models imported above
+# in database
 def create_db():
     db.create_all()
+    db.session.commit()
+
+# This method adds a Message object to the database
+def add_entry(entry):
+    db.session.add(entry)
+    db.session.commit()
+
+# This method removes an entry from the Messages table
+# by using its id
+def delete_by_id(entry_id):
+    obj = Messages.query.filter_by(id = entry_id)
+    db.session.delete(obj)
+
+# This method deletes all rows in Messages table
+# It returns the number of rows deleted
+def delete_all_entries():
+    num = db.session.query(Messages).delete()
+    db.session.commit()
+    return num
