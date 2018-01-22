8d0513ae1c57be8e59a27d56ea321c3a03eac9d6
Mauricio B de Paula <mauricio.browne@gmail.com>
2018-01-21 18:42:02 -0800

    Attempting to fix a bug with the real server.

--- server/server.py
+++ server/server.py
@@ -34,7 +34,7 @@ def getMessages():
         message_list = dbManager.get_all_entries()
         if message_list == None:
             return 'There was an error getting all entries.'
-        return message_list
+        return 'jsonify(message_list)'
     else:
         return 'There was an error.'
 
